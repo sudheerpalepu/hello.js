7350ce53636c47970401b8c2a609cd8a15f829ae
sudheerpalepu <146517155+sudheerpalepu@users.noreply.github.com>
2026-02-03 15:09:24 +0000

    commit

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
-alert("Hello World!");
+async function getData() {
+  try {
+    const response = await fetch('https://www.xe.com/currencycharts/');
+    if (!response.ok) throw new Error('Network error');
+
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getData();
